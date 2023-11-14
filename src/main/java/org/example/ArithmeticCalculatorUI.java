abcefdeeda64c2848af027ec95b9455b07df6bfc
sunghoo05 <hongna0418@gmail.com>
2023-11-13 19:06:56 -0500

    Add explaining comments for the first iteration code
    Sunghoo Hong

--- src/main/java/org/example/ArithmeticCalculatorUI.java
+++ src/main/java/org/example/ArithmeticCalculatorUI.java
@@ -13,10 +13,8 @@ package org.example;
 // import necessary libraries for UI design
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class ArithmeticCalculator
+public class ArithmeticCalculatorUI
 {
     // Variable 'textDisplay' is to display what the user inputs and the result of the calculation
     private final JTextField textDisplay;
@@ -26,7 +24,7 @@ public class ArithmeticCalculator
     private boolean startNewInput = true;
 
     // Calculator UI Design
-    public ArithmeticCalculator()
+    public ArithmeticCalculatorUI()
     {
         // Calculator Frame Design
         JFrame calculator = new JFrame("Calculator"); // initiate new JFrame
@@ -40,7 +38,28 @@ public class ArithmeticCalculator
         textDisplay.setHorizontalAlignment(JTextField.RIGHT); // set the location of the text in the field
         calculator.add(textDisplay, BorderLayout.NORTH); // add textDisplay to the JFrame created at the North of JFrame
 
-        // Buttons Layout Design
+        // Buttons Design
+        JPanel buttonPanel = new JPanel(); // initiate the button panel
+        buttonPanel.setLayout(new GridLayout(6,4)); // set the number of buttons
+        // Button Layout
+        String[] buttonLabels = {
+                "%", "CE", "C", "DEL",
+                "1/x", "x^2", "√", "/",
+                "7", "8", "9", "X",
+                "4", "5", "6", "-",
+                "1", "2", "3", "+",
+                "(-)", "0", ".", "="
+        }; // set the button layout
 
+        // Add Button Layout to buttonPanel
+        for (String label : buttonLabels)
+        {
+            JButton button = new JButton(label); // initiate to edit each button labels
+            button.setFont(new Font("default", Font.PLAIN, 25)); // set the size and font of button labels
+            button.addActionListener(new ButtonClickListener()); // enable buttons to do action based on the user's input
+            buttonPanel.add(button); // add button labels to the button panel
+        }
+
+        calculator.add(buttonPanel, BorderLayout.CENTER); // add buttonPanel to the JFrame and set the location of buttons
     }
 }
---
+++ src/main/java/org/example/ButtonClickListener.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ButtonClickListener implements ActionListener
+{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
