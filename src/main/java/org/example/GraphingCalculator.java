bde0ebfacef85c0c541f0569d444dd973fcecf93
sunghoo05 <hongna0418@gmail.com>
2023-11-14 01:23:31 -0500

    Add comments
    Sunghoo Hong

--- src/main/java/org/example/GraphingCalculator.java
+++ src/main/java/org/example/GraphingCalculator.java
@@ -11,6 +11,7 @@ public class GraphingCalculator
     private static GraphPlotter plotter; // Initiate plotter to plot the graph
     private static JTextField equationField; // Initiate a text field 'equationField'
     public static JPanel graphingCalculatorPanel; // Initiate a JPanel 'graphingCalculatorPanel'
+    public static JPanel regularCalculatorPanel; // Initiate a JPanel 'regularCalculatorPanel'
 
     public static void main(String[] args)
     {
@@ -27,6 +28,18 @@ public class GraphingCalculator
 
         // Graphing Calculator Panel
         graphingCalculatorPanel = new JPanel(new BorderLayout()); // Create a JPanel
+        setupGraphingCalculatorPanel(graphingCalculatorPanel); // Complete the set-up of the panel by using the method 'setupGraphingCalculatorPanel'
+        tabs.addTab("Graphing Calculator", graphingCalculatorPanel); // Add 'graphingCalculatorPanel' as one tab of the calculatorPanel
 
+        // Arithmetic Calculator Panel
+        regularCalculatorPanel = new JPanel(new BorderLayout()); // Create a JPanel
+        setupRegularCalculatorPanel(regularCalculatorPanel); // Complete the set-up of the panel by using the method 'setupRegularCalculatorPanel'
+        tabs.addTab("Regular Calculator", regularCalculatorPanel); // Add 'regularCalculatorPanel' as one tab of the calculatorPanel
+    }
+
+    private static void setupRegularCalculatorPanel(JPanel arithmeticCalculatorPanel) {
+    }
+
+    private static void setupGraphingCalculatorPanel(JPanel graphingCalculatorPanel) {
     }
 }
