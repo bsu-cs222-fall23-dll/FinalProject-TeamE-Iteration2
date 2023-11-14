f0e36c83af4e82c638dab8f7cd6d276e257da31b
sunghoo05 <hongna0418@gmail.com>
2023-11-14 12:32:14 -0500

    Add Unit Tests
    Sunghoo Hong

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("net.objecthunter:exp4j:0.4.8");
+    implementation("net.objecthunter:exp4j:0.4.8")
+    testImplementation("junit:junit:4.13.1");
 }
 
 tasks.test {
---
+++ src/test/java/org/example/GraphPlotterTest.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.awt.*;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class GraphPlotterTest {
+    private GraphPlotter plotter;
+
+
+    @Before
+    public void setUp() {
+        // Initialize GraphPlotter with a default size for testing
+        plotter = new GraphPlotter(new Dimension(600, 400));
+    }
+
+
+    @Test
+    public void testComputePlotPoints_LinearFunction() {
+        plotter.computePlotPoints("x", -10, 10, 1);
+        List<Point> points = plotter.getPointsToPlot();
+
+
+        assertNotNull("Points list should not be null", points);
+        assertFalse("Points list should not be empty", points.isEmpty());
+        assertEquals("Number of points", 21, points.size());
+
+
+        // Example: Check if the point at x = 0 is correctly calculated
+        // Assuming that the center of the graph (0,0) is mapped to the center of the screen
+        Point expectedCenterPoint = new Point(300, 200); // Adjust based on your coordinate conversion logic
+        assertTrue("Contains center point", points.contains(expectedCenterPoint));
+    }
+
+
+    // Additional tests can be written for other types of functions and edge cases
+}
